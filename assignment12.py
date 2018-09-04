2bac9ec295e41e0a522a438d540fc62e740588f5
pritbab <prithul1997@gmail.com>
2018-09-04 14:53:14 +0530

    Assignment12 done

---
+++ assignment12.py
@@ -0,0 +1,19 @@
+#question number1
+
+print("<--------solution 1-------->")
+from datetime import date
+days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+print(days[date.weekday(date.today())])
+
+
+#question number2
+
+print("<--------solution 2-------->")
+import webbrowser
+webbrowser.open_new_tab('https://www.youtube.com/watch?v=EUq7xrEmrV0')
+
+#question 3
+
+print("<--------solution 3-------->")
+import os
+os.rename('1.txt','1.jpg')
